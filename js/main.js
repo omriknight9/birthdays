e25a8efc91957dd0e61ee0ca1e582b5b5b3fee5b
Omri Shalev <omriknight9@gmail.com>
2019-09-09 17:45:57 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@ var d = new Date();
 var currentYear = d.getFullYear();
 
 $(document).ready(function (event) {
-    
-
+    for (var i = 1; i < 32; i++) {
+        var option = $('<option>', {
+            value: i,
+            text: i
+        }).appendTo($('#dayInput'))
+    }
 })
 
 function birthdays(name, day, month) {
@@ -13,6 +17,9 @@ function birthdays(name, day, month) {
     var nameInputVal = $('#nameInput').val();
     var dayInputVal = $('#dayInput').val();
     var monthInputVal = $('#monthInput').val();
+
+    console.log('dayInputVal: ' + dayInputVal);
+    console.log('monthInputVal: ' + monthInputVal);
 
     if (nameInputVal == '' || nameInputVal == undefined) {
         $('#nameInput').css({
@@ -50,40 +57,48 @@ function birthdays(name, day, month) {
     var nameCapitalized = nameInputVal.charAt(0).toUpperCase() + nameInputVal.slice(1);
 
     if (valid) {
-
         month = month - 1;
+        var year = currentYear
+        var date = new Date(year, month, day);
+        if (day == date.getDate()) {
 
-        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            var birthdayWrapper = $('<div>', {
+                class: 'birthdayWrapper',
+            }).appendTo($('.container')).hide().fadeIn('slow');
 
-        var birthdayWrapper = $('<div>', {
-            class: 'birthdayWrapper',
-        }).appendTo($('.container')).hide().fadeIn('slow');
+            var nameHeader = $('<h2>', {
+                class: 'nameHeader',
+                text: nameCapitalized
+            }).appendTo(birthdayWrapper)
 
-        var nameHeader = $('<h2>', {
-            class: 'nameHeader',
-            text: nameCapitalized
-        }).appendTo(birthdayWrapper)
+            var leftSide = $('<div>', {
+                class: 'left',
+            }).appendTo(birthdayWrapper);
 
-        var leftSide = $('<div>', {
-            class: 'left',
-        }).appendTo(birthdayWrapper);
+            var rightSide = $('<div>', {
+                class: 'right',
+            }).appendTo(birthdayWrapper);
 
-        var rightSide = $('<div>', {
-            class: 'right',
-        }).appendTo(birthdayWrapper);
+            for (var year = currentYear; year <= currentYear + 15; year++) {
+                var date2 = new Date(year, month, day);
+                buildBirthdays(leftSide, date2, year, days);
+            }
 
-        for (var year = currentYear; year <= currentYear + 15; year++) {
-            var date = new Date(year, month, day);
-            buildBirthdays(leftSide, date, year, days);
+            for (var year = currentYear + 16; year <= currentYear + 31; year++) {
+                var date3 = new Date(year, month, day);
+                buildBirthdays(rightSide, date3, year, days);
+            }
+
+            $('html, body').animate({ scrollTop: $('.birthdayWrapper:last-child').position().top }, 'slow'); 
+        } else {
+            $('#monthInput').css({
+                'border': '1px solid #FF4545'
+            });
+            $('#dayInput').css({
+                'border': '1px solid #FF4545'
+            });
         }
-
-        for (var year = currentYear + 16; year <= currentYear + 31; year++) {
-            var date = new Date(year, month, day);
-            buildBirthdays(rightSide, date, year, days);
-        }
-
-        $('html, body').animate({ scrollTop: $('.birthdayWrapper:last-child').position().top }, 'slow');
-       
     }
 }
 
